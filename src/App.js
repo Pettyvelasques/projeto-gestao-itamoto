832eea2ccfe3fd1586bbfcc1a86c51a36c7a3eba
Patrick Velasques <pettyvelasques@hotmail.com>
2022-08-30 00:13:42 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './components/pages/Home';
+import Pecas from './components/pages/Pecas'
+import NovaCompra from './components/pages/NovaCompra'
+import Clientes from './components/pages/Clientes'
+import NovoCliente from './components/pages/NovoCliente'
+import Vendas from './components/pages/Vendas'
+import NovaVenda from './components/pages/NovaVenda'
+import Financeiro from './components/pages/Financeiro'
+
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pecas" element={<Pecas />} />
+          <Route path="/novacompra" element={<NovaCompra />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/novocliente" element={<NovoCliente />} />
+          <Route path="/vendas" element={<Vendas />} />
+          <Route path="/novavenda" element={<NovaVenda />} />
+          <Route path="/financeiro" element={<Financeiro />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+
+import styles from './Navbar.module.css'
+import Container from './Container'
+
+import Logo from '../../img/itamoto_logo.png'
+
+function Navbar() {
+    return (
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to="/">
+                    <img src={Logo} alt="Mechanic Tools Logo" />
+                </Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/pecas">Peças</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/clientes">Clientes</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to="/vendas">Vendas</Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
+    )
+}
+
+
+export default Navbar   
